c2f29ef3d0769e1ef429c53f7bd13872ae49bb86
Dylan Cummins <36158077+Dillonius@users.noreply.github.com>
2024-05-14 22:49:22 +0100

    testing deployment

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/api', (req, res) => {
+    res.send('Hello, API!');
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
