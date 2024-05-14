ce566964989d35ef3434f5fe37d9f41397ca0a70
Dylan Cummins <36158077+Dillonius@users.noreply.github.com>
2024-05-14 23:12:52 +0100

    moongoDB connection test

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 8080;
 
+const UserSchema = new mongoose.Schema({
+    _id: ObjectId,
+    name: String,
+    email: String,
+    password: String
+});
+
+const UserModel = mongoose.model('users', UserSchema);
+
+mongoose.connect('mongodb+srv://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/users');
 // Define your API routes here
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -10,6 +22,14 @@ app.get('/', (req, res) => {
 
 app.get('/api', (req, res) => {
     res.send('Hello, API!');
+});
+
+app.get('/getUsers', (req, res) => {
+    UserModel.find({}).then(function(users) {
+        res.json(users);
+    }).catch(function(err){
+        console.log(err);
+    });
 });
 
 // Start the server
