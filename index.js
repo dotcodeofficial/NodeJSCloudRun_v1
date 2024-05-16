94c2a6a8e78c3eeb177162439a0c33d9bcf1270c
Dylan Cummins <36158077+Dillonius@users.noreply.github.com>
2024-05-16 12:47:48 +0100

    init

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
 const express = require('express');
-const { ObjectId } = require('mongodb');
-const mongoose = require('mongoose');
 
 const app = express();
 const port = 8080;
 
-mongoose.connect('mongodb+srv://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/users');
-
-const UserSchema = new mongoose.Schema({
-    _id: ObjectId,
-    name: String,
-    email: String,
-    password: String
-},
-{ collection: 'users' });
-
-const UserModel = mongoose.model('users', UserSchema);
-
-app.get('/getUsers', (req, res) => {
-    UserModel.find({}).then(function(users) {
-        res.json(users);
-    }).catch(function(err){
-        console.log(err);
-    });
-});
 
 
 // Define your API routes here
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Helloooopo, World!');
 });
 
 app.get('/api', (req, res) => {
