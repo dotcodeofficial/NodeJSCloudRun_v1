afa40e5aaf5c2e32bbe84ca90e960a9e01aa5817
Dylan Cummins <36158077+Dillonius@users.noreply.github.com>
2024-05-17 15:34:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,37 +3,30 @@ const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 //const uri = "mongodb://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&directConnection=true";
-const prefix= "TuLNp6TobxA2yPKY";
+const prefix= "TuLNp6TobxA2yPKY - ";
 const app = express();
 const port = 8080;
-console.log(prefix + "Test!");
+
 const client = new MongoClient(uri);
-console.log(prefix + "Client Created");
 client.connect();
-console.log(prefix + "Client Connected");
+const movies = client.db("sample_mflix").collection("movies");
   async function run() {
     try {
-      
-      const database = client.db("sample_mflix");
-      console.log(prefix + "database variable connected");
-      const movies = database.collection("movies");
-      console.log(prefix + "movies variable connected");
+
       const query = { title: "Room" };
-      console.log(prefix + "query built");
       const options = {
         sort: { "imdb.rating": -1 },
-
         projection: { _id: 0, title: 1, imdb: 1 },
       };
-      console.log(prefix + "options set");
 
       const movie = await movies.findOne(query, options);
       console.log(movie);
       return movie;
     }
     catch (e) {
-      console.error(prefix+e);
+      console.error(prefix+" error "+e);
     }
     finally {
     }
@@ -42,7 +35,9 @@ console.log(prefix + "Client Connected");
 
 app.get('/getUsers', (req, res) => {
 
-    run().then(foo => res.send(foo)).catch(err => console.log(err));
+    run().then(foo => {
+      res.send(foo);
+    }).catch(err => console.log(err));
 });
 
 // Define your API routes here
