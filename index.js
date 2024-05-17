76b50b1797145577b1688a7c443c4cf0a0f6f482
Dylan Cummins <36158077+Dillonius@users.noreply.github.com>
2024-05-17 14:23:13 +0100

    update

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const express = require('express');
 const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
 const port = 8080;
 
-mongoose.connect('mongodb+srv://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/users');
+const client = new MongoClient(uri);
 
-const UserSchema = new mongoose.Schema({
-    _id: ObjectId,
-    name: String,
-    email: String,
-    password: String
-},
-{ collection: 'users' });
+  async function run() {
+    try {
+        
+      // Send a ping to confirm a successful connection
+      const database = client.db("sample_mflix");
+      const movies = database.collection("movies");
+      // Query for a movie that has the title 'The Room'
+      const query = { title: "Room" };
 
-const UserModel = mongoose.model('users', UserSchema);
+      const options = {
+        // Sort matched documents in descending order by rating
+        sort: { "imdb.rating": -1 },
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { _id: 0, title: 1, imdb: 1 },
+      };
+
+      const movie = await movies.findOne(query, options);
+      console.log(movie);
+      return movie;
+    }
+    finally {
+    }
+  }
+
 
 app.get('/getUsers', (req, res) => {
-    UserModel.find({}).then(function(users) {
-        res.json(users);
-    }).catch(function(err){
-        console.log(err);
-    });
-});
 
+    run().then(foo => res.send(foo)).catch(err => console.log(err));
+});
 
 // Define your API routes here
 app.get('/', (req, res) => {
