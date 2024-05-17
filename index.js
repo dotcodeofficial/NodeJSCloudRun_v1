77eed2f0b07539596da568791d98cf86963b3d78
Dylan Cummins <36158077+Dillonius@users.noreply.github.com>
2024-05-17 15:19:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,29 +4,36 @@ const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 //const uri = "mongodb://admin:hekslrqaF8sBd5Vc@cluster0.q9hq99b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&directConnection=true";
-
+const prefix= "TuLNp6TobxA2yPKY";
 const app = express();
 const port = 8080;
-console.log("Test!");
+console.log(prefix + "Test!");
 const client = new MongoClient(uri);
+console.log(prefix + "Client Created");
 client.connect();
+console.log(prefix + "Client Connected");
   async function run() {
     try {
       
       const database = client.db("sample_mflix");
+      console.log(prefix + "database variable connected");
       const movies = database.collection("movies");
-
+      console.log(prefix + "movies variable connected");
       const query = { title: "Room" };
-
+      console.log(prefix + "query built");
       const options = {
         sort: { "imdb.rating": -1 },
 
         projection: { _id: 0, title: 1, imdb: 1 },
       };
+      console.log(prefix + "options set");
 
       const movie = await movies.findOne(query, options);
       console.log(movie);
       return movie;
+    }
+    catch (e) {
+      console.error(prefix+e);
     }
     finally {
     }
